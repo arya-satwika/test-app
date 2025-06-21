f60e58dd6d4b9b001a9064ea937530e60953c67c
arya satwika <aryastwk95@gmail.com>
2025-06-21 22:11:36 +0700

    initial commit

---
+++ src/components/Main.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import caught from '../assets/caught.png';
+
+</script>
+<div class= "py-5">
+    <div 
+        class = "py-2 mx-auto w-70 h-60 bg-center bg-no-repeat bg-slate-300 rounded-4xl"
+        style = "background-image: url({caught})"
+        >
+    </div>
+    <div class= "text-center py-3 text-3xl font-bold">PIPISS!!</div>
+    
+</div>
