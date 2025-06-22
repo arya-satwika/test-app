10af45f2fbcb538120d15cb7a7e1f0a7bbbd375d
arya satwika <aryastwk95@gmail.com>
2025-06-22 10:17:55 +0700

    refactor Header and Main components for improved structure and accessibility

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,12 +3,12 @@
 
 <div class="flex flex-row justify-evenly bg-blue-500 p-5">
     <h1 class="text-3xl font-bold">Your Name here..</h1>
-    <div class="flex flex-row min-h-full items-center px-5 gap-x-3">
+    <span class="flex flex-row min-h-full px-5 gap-x-3">
         <i class="fa-solid fa-face-smile text-3xl"></i>
-        <a class="fa-solid fa-heart text-3xl cursor-select" href=" https://www.youtube.com/watch?v=phiq7Pp5mpU&list=LL&index=1" target="_blank"></a>
+        <a class="fa-solid fa-heart text-3xl cursor-select" href=" https://www.youtube.com/watch?v=phiq7Pp5mpU&list=LL&index=1" target="_blank" aria-label=k></a>
         <i class="fa-solid fa-gear text-3xl"></i>
         <i class="fa-solid fa-moon text-3xl"></i>
-    </div> 
+    </span>     
     <div class="ml-auto min-h-full flex items-center">
         <h1 >About me</h1>
     </div>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -2,6 +2,8 @@
     import caught from '../assets/caught.png';
 
 </script>
+
+
 <div class= "py-5">
     <div 
         class = "py-2 mx-auto w-70 h-60 bg-center bg-no-repeat bg-slate-300 rounded-4xl"
@@ -9,5 +11,29 @@
         >
     </div>
     <div class= "text-center py-3 text-3xl font-bold">PIPISS!!</div>
-    
+    <section id="about">
+        <h1 class = "text-center p-4 font-bold text-4xl">dis the about section</h1>
+        <div class = "mx-auto flex flex-row justify-center gap-x-10">
+            
+            <article class="text-xl bg-slate-500 p-2 rounded-2xl min-h-30 max-w-60 w-60 content-center">
+                <h1 class="text-center font-bold">this is awesome</h1>
+                <p class="text-center">I'm learning Svelte and it's amazing!</p>
+            </article>
+            
+            <article class="text-xl bg-slate-500 p-5 rounded-2xl min-h-30 max-w-60 w-60 content-center">
+                <h1 class="text-2xl font-bold text-center">About me</h1>
+                <p class="text-sm text-wrap text-center">I am a software developer with a passion for creating interactive web applications.</p>
+            </article>
+
+            <article class="text-xl bg-slate-500 p-5 rounded-2xl min-h-30 max-w-60 w-60 content-center">
+                <h1 class="text-2xl font-bold text-center">Contact</h1>
+                <p class="text-sm text-wrap text-center">You can reach me at 
+                    <a class = "text-xl font-bold text-violet-300" href="https://t3.ftcdn.net/jpg/02/95/44/22/360_F_295442295_OXsXOmLmqBUfZreTnGo9PREuAPSLQhff.jpg" target="_blank">
+                        <br>my penis
+                    </a> 
+                </p>
+            </article>
+
+        </div>
+    </section>
 </div>
